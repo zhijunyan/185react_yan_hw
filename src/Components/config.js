62008920210f58be6a4219f6b7d0f787f06c5f7c
zhijunyan <zhijunyan@umail.ucsb.edu>
2020-05-07 12:32:01 -0700

    Delete config.js

--- src/Components/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-    apiKey: "AIzaSyAJ_LijpIbHkRaehblCpQcy_0KCtBvTNq0",
-    authDomain: "cs185-yan.firebaseapp.com",
-    databaseURL: "https://cs185-yan.firebaseio.com",
-  }
-export default config;
